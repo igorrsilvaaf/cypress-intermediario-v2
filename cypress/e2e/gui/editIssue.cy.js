ce65eb5bb2443d838ad98e4780a51554ec90d9ac
Igor Silva <igorqa2022@outlook.com>
2024-03-25 19:57:15 -0300

    Continuacao do curso

---
+++ cypress/e2e/gui/editIssue.cy.js
@@ -0,0 +1,17 @@
+const options = {env: {snapshotOnly: true}}
+
+describe('Edit issue via GUI', options, () => {
+    const issue = {
+        title: 'Minha Nova Issue',
+        description: 'Esta é uma descrição da minha nova issue.',
+        project: {
+          name: 'Meu Novo Projeto com Cypress',
+          description: 'Este é o meu novo projeto para testes.'
+        }
+      }
+
+      const label = {
+        name: ,
+        color: 
+      }
+});
